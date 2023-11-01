5033717ccdf72b373e98b449e51ecabfd07a0a49
KeatonShawhan <64229449+KeatonShawhan@users.noreply.github.com>
2023-11-01 10:55:30 -0700

    finishing  creative tilt

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 Endless Runner: Stick Run
 Name: Keaton Shawhan
 Date: 11/6/23
-Hours spent: 8
+Hours spent: 20-23
 Points hit (15/15):
   Use multiple Scene classes (dictated by your game's style) (1)
   Have some form of player input/control appropriate to your game design (1)
@@ -25,6 +25,15 @@ Points hit (15/15):
   https://freesound.org/people/tim.kahn/sounds/44876/
   https://freesound.org/people/JustInvoke/sounds/138490/
   https://freesound.org/people/Eponn/sounds/619835/
+
+  Creative Tilt:
+  1. I think that my game does do something technically interesting in the sense that each platform feels important.
+     I'm particularly proud of my implementation of jumping onto a platform and the object changing the color of the background.
+     This and the physics part of my game helped me learn something new because there was a lot of bug-fixing and testing throughout development.
+
+  2. When it comes to visual style, I think that my game has some of the best. The fact that each platform changes the color of the background to what it was originally feels really cool when you are playing.
+     Also I think that the audios that I found fit in really nicely, especially ones that play when you die or lose a life.
+     Lastly, I personally believe this is a clever take on the endless runner form because most of them have an "instant death" way of ending the game, whether thats jumping into a hole or something else, I think that the idea of not missing a platform feels unique and still plays like an endless runner. 
 */
 
 "use strict"
